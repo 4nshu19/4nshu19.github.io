6d6b8058b711d464f9f841743f68c677b97fa1a3
Anshu Rathee <4nshu19@gmail.com>
2017-07-12 19:04:42 +0530

    added papaparse function

--- files/myscript.js
+++ files/myscript.js
@@ -1,6 +1,21 @@
 
-Papa.parse(../data/deliveries.csv, {
-	complete: function(results) {
-		console.log("Finished:", results.data);
-	}
-});
+console.log("ready");
+
+function parseData(createGraph) {
+
+
+  Papa.parse("../data/matches.csv", {
+      download: true,
+      complete: function(results) {
+  		console.log("Finished:", results.data);
+      createGraph(results.data);
+
+  	}
+  });
+}
+
+function createGraph(data){
+
+}
+
+parseData(createGraph);
