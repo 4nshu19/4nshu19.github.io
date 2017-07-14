f2123d28151a2efebab24ec92ccbbba45b8ae9b1
Anshu Rathee <4nshu19@gmail.com>
2017-07-14 15:19:02 +0530

    code splitting

---
+++ files/creategraph.js
@@ -0,0 +1,49 @@
+function createGraph(data,x){
+
+  // for(var i=1; i< data.length-1; i++)
+  //   {console.log(data[i]);}
+
+
+  var chart = c3.generate({
+      data: {
+          columns: [
+              x  //s8
+          ]
+      },
+      axis: {
+          x: {
+              type: 'category',
+              categories: s8data,
+              show: false
+          }
+      }
+  });
+
+
+var avg_byrun=0, avg_bywic=0;
+for(var i=1; i< data.length - 1 ; i++)
+     {avg_byrun = avg_byrun + parseInt(data[i][11]);
+      avg_bywic =  avg_bywic + parseInt(data[i][12]);}
+
+avg_byrun = avg_byrun / (data.length-1);
+avg_bywic = avg_bywic / (data.length-1);
+
+var chart = c3.generate({
+  bindto: '#chart2',
+  data: {
+      columns: [
+          ['Average win by runs', avg_byrun],
+          ['Average win by wickets', avg_bywic]
+      ],
+      type: 'bar'
+  },
+  bar: {
+      width: {
+          ratio: 0.5 // this makes bar width 50% of length between ticks
+      }
+      // or
+      //width: 100 // this makes bar width 100px
+  }
+});
+
+}
--- files/myscript.js
+++ files/myscript.js
@@ -10,15 +10,15 @@ function parseData() {
       complete: function(results) {
   		console.log("Finished:", results.data);
       createArray(results.data);
-      createGraph(results.data);
+      createGraph(results.data, s8);
 
   	}
   });
 }
 
-
 function createArray(data)
-{ //   s8=[];s9=[];s10=[];s11=[];s12[];s13=[];s14=[];s15=[];s16=[];
+{  s8=['Win By Runs'],s9=['Win By Runs'],s10=['Win By Runs'],s11=['Win By Runs'],s12=['Win By Runs'],s13=['Win By Runs'],s14=['Win By Runs'],s15=['Win By Runs'],s16=['Win By Runs'];
+
    for(var i=1; i< data.length-1; i++)
     { var win = data[i][4] + " vs " + data[i][5] + "..  Win = " + data[i][10] ;
      if(parseInt(data[i][1]) == 2008)
@@ -61,54 +61,14 @@ function createArray(data)
 }
 
 
-function createGraph(data){
-
-  // for(var i=1; i< data.length-1; i++)
-  //   {console.log(data[i]);}
 
 
-  var chart = c3.generate({
-      data: {
-          columns: [
-              s8
-          ]
-      },
-      axis: {
-          x: {
-              type: 'category',
-              categories: s8data,
-              show: false
-          }
-      }
-  });
+new Vue({
+ el: '#btn1',
+ methods: {
+   graph()
+   {
+   parseData();}
+ }
 
-
-var avg_byrun=0, avg_bywic=0;
-for(var i=1; i< data.length - 1 ; i++)
-     {avg_byrun = avg_byrun + parseInt(data[i][11]);
-      avg_bywic =  avg_bywic + parseInt(data[i][12]);}
-
-avg_byrun = avg_byrun / (data.length-1);
-avg_bywic = avg_bywic / (data.length-1);
-
-var chart = c3.generate({
-  bindto: '#chart2',
-  data: {
-      columns: [
-          ['Average win by runs', avg_byrun],
-          ['Average win by wickets', avg_bywic]
-      ],
-      type: 'bar'
-  },
-  bar: {
-      width: {
-          ratio: 0.5 // this makes bar width 50% of length between ticks
-      }
-      // or
-      //width: 100 // this makes bar width 100px
-  }
 });
-
-}
-
-parseData();
