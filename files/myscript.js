20d3326d113980df4f8dc08717d491fb0f2fc557
Anshu Rathee <4nshu19@gmail.com>
2017-07-14 14:56:06 +0530

    added for diff seasons

--- files/myscript.js
+++ files/myscript.js
@@ -2,7 +2,7 @@
 var s8=['Win By Runs'],s9=['Win By Runs'],s10=['Win By Runs'],s11=['Win By Runs'],s12=['Win By Runs'],s13=['Win By Runs'],s14=['Win By Runs'],s15=['Win By Runs'],s16=['Win By Runs'];
 var s8data=[],s9data=[],s10data=[],s11data=[],s12data=[],s13data=[],s14data=[],s15data=[],s16data=[];
 
-function parseData(createGraph) {
+function parseData() {
 
 
   Papa.parse("../data/matches.csv", {
@@ -25,6 +25,38 @@ function createArray(data)
         {s8.push(parseInt(data[i][11]));
          s8data.push(win);
          }
+        else if(parseInt(data[i][1]) == 2009)
+            {s9.push(parseInt(data[i][11]));
+             s9data.push(win);
+             }
+             else if(parseInt(data[i][1]) == 2010)
+                 {s10.push(parseInt(data[i][11]));
+                  s10data.push(win);
+                  }
+                  else if(parseInt(data[i][1]) == 2011)
+                      {s11.push(parseInt(data[i][11]));
+                       s11data.push(win);
+                       }
+                       else if(parseInt(data[i][1]) == 2012)
+                           {s12.push(parseInt(data[i][11]));
+                            s12data.push(win);
+                            }
+                            else if(parseInt(data[i][1]) == 2013)
+                                {s13.push(parseInt(data[i][11]));
+                                 s13data.push(win);
+                                 }
+                                 else if(parseInt(data[i][1]) == 2014)
+                                     {s14.push(parseInt(data[i][11]));
+                                      s14data.push(win);
+                                      }
+                                      else if(parseInt(data[i][1]) == 2015)
+                                          {s15.push(parseInt(data[i][11]));
+                                           s15data.push(win);
+                                           }
+                                           else if(parseInt(data[i][1]) == 2016)
+                                               {s16.push(parseInt(data[i][11]));
+                                                s16data.push(win);
+                                                }
     }
 }
 
@@ -44,7 +76,8 @@ function createGraph(data){
       axis: {
           x: {
               type: 'category',
-              categories: s8data
+              categories: s8data,
+              show: false
           }
       }
   });
@@ -78,4 +111,4 @@ var chart = c3.generate({
 
 }
 
-parseData(createGraph);
+parseData();
